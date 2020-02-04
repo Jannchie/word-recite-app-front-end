f445a3e1cc62fc7f12b1457da9a973f0f9c31d1b
jannchie <jannchie@gmail.com>
2020-02-04 19:49:07 +0900

    ADD: global data

---
+++ src/data/index.js
@@ -0,0 +1,15 @@
+export default {
+  user: {
+    username: undefined,
+    password: undefined,
+    wordList: undefined
+  },
+  searchText: "",
+  login: undefined,
+  wordList: [],
+  alert: {
+    message: undefined,
+    type: undefined,
+    display: false
+  }
+};
