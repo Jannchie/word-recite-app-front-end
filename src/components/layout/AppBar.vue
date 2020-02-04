285a53533fa4ab3d24f79efcfebddfb64221b83c
jannchie <jannchie@gmail.com>
2020-02-04 19:48:49 +0900

    UPDATE: layout

---
+++ src/components/layout/AppBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar app>
+    <v-toolbar-title>Page title</v-toolbar-title>
+    <v-text-field
+      solo
+      v-model="searchText"
+      dense
+      rounded
+      flat
+      hide-details
+      label="搜索"
+      prepend-inner-icon="mdi-magnify"
+    />
+  </v-app-bar>
+</template>
+<script>
+import data from "../../data";
+export default {
+  data() {
+    return { ...data };
+  },
+  watch: {
+    searchText() {
+      console.log(this.searchText);
+    }
+  }
+};
+</script>
---
+++ src/components/layout/BottomNav.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-bottom-navigation color="primary" shift app v-model="bottomNav" grow>
+    <v-btn to="wordList" value="wordList">
+      <span>词单广场</span>
+      <v-icon>mdi-book-multiple</v-icon>
+    </v-btn>
+    <v-btn to="word" value="word">
+      <span>单词查询</span>
+      <v-icon>mdi-book-search</v-icon>
+    </v-btn>
+    <v-btn to="user" value="user">
+      <span>个人中心</span>
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      bottomNav: "wordList"
+    };
+  }
+};
+</script>
+<style>
+.v-item-group.v-bottom-navigation .v-btn.v-size--default {
+  height: inherit;
+  min-width: 80px;
+  padding: inherit;
+}
+</style>
