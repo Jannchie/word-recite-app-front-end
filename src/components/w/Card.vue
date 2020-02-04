63b11d28f065bc67065fecb66a6cf38d5fae5b62
jannchie <jannchie@gmail.com>
2020-02-04 19:49:41 +0900

    ADD: custom components

---
+++ src/components/w/Card.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-card class="w-card elevation-10">
+    <slot></slot>
+  </v-card>
+</template>
+<style scoped>
+.v-application .w-card {
+  border-radius: 24px;
+}
+</style>
---
+++ src/components/w/Notification.vue
@@ -0,0 +1,34 @@
+<template>
+  <VSnackbar v-model="snackbar" top :color="color" style="z-index:10">
+    {{ message }}
+    <VBtn color="write" text @click.stop="display = false">
+      close
+    </VBtn>
+  </VSnackbar>
+</template>
+<script>
+import data from "../../data";
+export default {
+  data() {
+    return data.alert;
+  },
+  computed: {
+    snackbar: {
+      get() {
+        return this.display;
+      },
+      set(val) {
+        this.display = val;
+      }
+    },
+    color() {
+      return this.type;
+    }
+  }
+};
+</script>
+<style>
+.v-snack__content {
+  background: linear-gradient(60deg, #ffffff44, #ffffff11) !important;
+}
+</style>
